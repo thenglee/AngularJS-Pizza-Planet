2961533ce8a1a33dd118ebcb994cada8e2689999
Tan Theng Lee <thengleetan@gmail.com>
2015-04-10 14:59:56 +0800

    Display appetizers data

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,6 +11,11 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
     }
   ];
 
